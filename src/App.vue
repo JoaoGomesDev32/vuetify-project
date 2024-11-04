8b6991196c122b1359abef55370b7cf13f40c1d4
João Silva <joaogomesdev32@gmail.com>
2024-11-04 23:02:28 +0000

    create project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <VApp>
+    <VAppBar flat color="amber">
+      <VAppBarTitle>Reparauto</VAppBarTitle>
+    </VAppBar>
+    <!--cor primaria = amber secundaria = red-accent-4-->
+    <VMain>
+      
+      <VContainer>
+        <h1>Denize Santos</h1>
+        <div class="text-subtitle">Representante Comercial</div>
+      </VContainer>
+      <!-- <RouterView /> -->
+    </VMain>
+  </VApp>
+</template>
+
+<script setup>
+  //
+</script>
